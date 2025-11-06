abacbc085044c0edaaba36718d86c099575276f8
Sam Huggins <samhuggins@Sams-MacBook-Air-8.local>
2025-11-06 16:20:12 +0000

    more control - somehow with less control

--- control.py
+++ control.py
@@ -12,15 +12,13 @@ import pinocchio as pin
 from bezier import Bezier
     
 # in my solution these gains were good enough for all joints but you might want to tune this.
-Kp = 275.               # proportional gain (P of PD)
+Kp = 20               # proportional gain (P of PD)
 Kv = 2 * np.sqrt(Kp)   # derivative gain (D of PD)
 
 def controllaw(sim, robot, trajs, tcurrent, cube):
     q, vq = sim.getpybulletstate()
     
     t_idx = (int) (tcurrent / dt)
-
-    print(t_idx)
 
     q_desired = trajs[t_idx][0][:]
     v_desired = trajs[t_idx][1][:]      # your array second dimension is actually velocity
@@ -36,81 +34,65 @@ def controllaw(sim, robot, trajs, tcurrent, cube):
 
     tau = pin.rnea(robot.model, robot.data, q, vq, a)
 
+    print("tau:", tau)
+
     sim.step(tau)
     
-    # # From trajs get torques
-    # q_current = trajs[(int) (tcur/dt)][0][:]
-    # vq_current = trajs[(int) (tcur/dt)][1][:]
-
-    # torques = [0.0 for _ in sim.bulletCtrlJointsInPinOrder]
-    # sim.step(torques)
+    
 
 def controllawBezier(sim, robot, trajs, tcurrent, cube):
-    q, vq = sim.getpybulletstate()
+    q, v = sim.getpybulletstate()
     
     t_idx = (int) (tcurrent)
+    print("t_idx", t_idx)
 
+    b = trajs
 
-    q_desired = trajs[t_idx][0].eval_horner(DT)
-    v_desired = trajs[t_idx][1].eval_horner(DT)
-    a_desired = trajs[t_idx][2].eval_horner(DT)
+    q_desired = b(tcurrent)
+    v_desired = b.derivative(1)(tcurrent)
+    a_desired = b.derivative(2)(tcurrent)
     
-    print(q_desired, "\t", v_desired, "\t", a_desired)
 
     # PD correction (optional but recommended)
-    pos_err = q_desired - q
-    vel_err = v_desired - vq
+    a_cmd = a_desired #+ Kp*(q_desired - q) + Kv*(v_desired - v)
+    tau_des = pin.rnea(robot.model, robot.data, q_desired, v_desired, a_desired)
+    tau_err = Kp * (q_desired - q) + Kv * (v_desired - v) 
+    tau = tau_des + tau_err
 
-    a = a_desired + Kp*pos_err + Kv*vel_err
-
-    tau = pin.rnea(robot.model, robot.data, q, vq, a)
-
-    q_current = pin.integrate(robot.model, q_desired, tau*DT)
-
-    data = robot.data.copy()  # Fresh data instance
-    pin.forwardKinematics(robot.model, data, q_current)
-    pin.updateFramePlacements(robot.model, data)  # Important!
+    print("tau:\n", tau)
 
     sim.step(tau)
+
+    return tau
+
     
-    
+
 if __name__ == "__main__":
         
-    from tools import setupwithpybullet, setupwithpybulletandmeshcat, rununtil
+    from tools import setupwithpybullet, setupwithpybulletandmeshcat, rununtil, setupwithmeshcat
     from config import DT
+    import time
     
-    robot, sim, cube = setupwithpybullet()
+    robotsim, sim, cubesim = setupwithpybullet()
+    robot, cube, viz = setupwithmeshcat(url="tcp://127.0.0.1:6003")
     
     
     from config import CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET    
     from inverse_geometry import computeqgrasppose
-    from path import computepath
+    from path import computepath, displaypath
     from trajectory import getTrajectory1, getTrajBezier
     
-    q0,successinit = computeqgrasppose(robot, robot.q0, cube, CUBE_PLACEMENT, None)
-    qe,successend = computeqgrasppose(robot, robot.q0, cube, CUBE_PLACEMENT_TARGET,  None)
-    path = computepath(robot, cube, q0, qe, CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET)
+    q0,successinit = computeqgrasppose(robotsim, robotsim.q0, cubesim, CUBE_PLACEMENT, None)
+    qe,successend = computeqgrasppose(robotsim, robotsim.q0, cubesim, CUBE_PLACEMENT_TARGET,  None)
+    path = computepath(robotsim, cubesim, q0, qe, CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET)
 
     #setting initial configuration
     sim.setqsim(q0)
     
     
-    #TODO this is just an example, you are free to do as you please.
-    #In any case this trajectory does not follow the path 
-    #0 init and end velocities
-    def maketraj(q0,q1,T): #TODO compute a real trajectory !
-        q_of_t = Bezier([q0,q0,q1,q1],t_max=T)
-        vq_of_t = q_of_t.derivative(1)
-        vvq_of_t = vq_of_t.derivative(1)
-        return q_of_t, vq_of_t, vvq_of_t
-    
-    P, I, D = Kp, 1, Kv
-    
-    
-    #TODO this is just a random trajectory, you need to do this yourself
-    
+    P, I, D = Kp, 1, Kv    
     max_time = 10
-    num_of_steps = 10
+    num_of_steps = 100
     
     # trajs, time_steps = getTrajectory1(robot, cube, path, max_time, P, I, D)
     trajs, time_steps = getTrajBezier(robot, cube, path, max_time, num_of_steps)
@@ -123,18 +105,23 @@ if __name__ == "__main__":
 
     dt = max_time / num_of_steps
 
-    print("traj",len(trajs))
+    # print("traj",len(trajs))
+
+    displaypath(robot, cube, path, 1/500, viz)
     
     
     while tcur < total_time:
-        print("tcur:", tcur, "\tt_total:", total_time)
-        # print("control loop iteration:", (int) (tcur/dt), "of", (int) (total_time/dt))
+    #     # print("tcur:", tcur, "\tt_total:", total_time)
+    #     # print("control loop iteration:", (int) (tcur/dt), "of", (int) (total_time/dt))
 
-        # rununtil(controllaw, dt, sim, robot, trajs, tcur, cube)
-        # tcur += dt
+    #     # rununtil(controllaw, dt, sim, robot, trajs, tcur, cube)
+    #     # tcur += dt
+        
+        controllawBezier(sim, robotsim, trajs, tcur, cubesim)
 
-        rununtil(controllawBezier, DT*1000, sim, robot, trajs, tcur, cube)
+    #     # rununtil(controllawBezier, DT*1000, sim, robot, trajs, tcur, cube)
         tcur += dt
+        time.sleep(0.01)
     
     
     
--- trajectory.py
+++ trajectory.py
@@ -87,16 +87,25 @@ def getTrajBezier(robot, cube, path, max_time, num_of_steps):
     time_steps = np.arange(t_start, t_end+step_size, step_size)
 
 
-    short_path = getShortenedPath(path, num_of_steps)
+    bezierPoints = getBezierPoints(path, num_of_steps)
 
     trajs = []
 
-    for i in range(len(time_steps)-1):
-        trajs += [makeBezTraj(short_path[i], short_path[i+1], 
-                              time_steps[i], time_steps[i] + step_size)]
-
-    
+    # Make one single Bezier curve with multiple points in it from getShortenedPath
+    # Then iterate through time steps in the control law!!!!!
+    trajs = Bezier(bezierPoints, 0, max_time)
+        
     return trajs, time_steps
+
+
+def getBezierPoints(path, num_of_steps):
+    bezierPath = np.empty([num_of_steps+3, len(path[0])])
+    bezierPath[0] = path[0]
+    bezierPath[1:-1] = getShortenedPath(path, num_of_steps)
+    bezierPath[-1] = path[-1]
+    
+    return bezierPath
+
 
 def getShortenedPath(path, num_of_steps):
     '''
@@ -104,10 +113,7 @@ def getShortenedPath(path, num_of_steps):
     '''
     shortened_path = np.empty([num_of_steps+1, len(path[0])])
     for i in range(num_of_steps):
-        if i == 0:
-            path_idx = 0
-        else:
-            path_idx = (int) ((len(path) / num_of_steps) * i)
+        path_idx = (int) ((len(path) / num_of_steps) * i)
         shortened_path[i] = path[path_idx]
 
     shortened_path[-1] = path[-1]
@@ -115,11 +121,9 @@ def getShortenedPath(path, num_of_steps):
     return shortened_path
 
 
-def makeBezTraj(q0,q1,t0, t1):
-        q_of_t = Bezier([q0,q0,q1,q1], t_min=t0, t_max=t1)
-        vq_of_t = q_of_t.derivative(1)
-        vvq_of_t = vq_of_t.derivative(1)
-        return q_of_t, vq_of_t, vvq_of_t
+def makeBezTraj(q0,q1,t0, t1, mt):
+        b = Bezier([q0,q0,q1,q1], t0, t1, mt)
+        return b
 
     
 
@@ -141,6 +145,7 @@ if __name__ == "__main__":
     from config import CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET
     from inverse_geometry import computeqgrasppose
     from path import computepath, displaypath
+    from bezier import Bezier
     
     robot, cube, viz = setupwithmeshcat(url="tcp://127.0.0.1:6003")
     
@@ -159,16 +164,9 @@ if __name__ == "__main__":
     P, I, D = 10, 1, 1
     max_time = 10
     num_of_steps = 3
+    step_size = max_time / num_of_steps
 
     # trajs, time_steps = getTrajectory1(robot, cube, path, max_time, P, I, D)
 
     trajs, time_steps = getTrajBezier(robot, cube, path, 10, num_of_steps)
 
-    
-    # print(len(path))
-    # print(len(time_steps))
-    # print(len(trajs))
-
-    # print(trajs[1])
-    # print(path[0])
-    # print(path[0]==path[1])
