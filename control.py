12336e90403e356b2ba07523977b0fb32bd76df2
Sam Huggins <samhuggins@Sams-MacBook-Air-8.local>
2025-11-09 18:50:09 +0000

    Got control working. There are two implementations there, controllawHighPID, which works fairly well, though uses high PID values to brute force gavity, and controllaw, which does not work, but has some tau't that include gravity, though the gravity doesn't get calculated properly for some reason. Can't figure out why that is...

--- control.py
+++ control.py
@@ -8,120 +8,235 @@ Created on Wed Sep  6 15:32:51 2023
 
 import numpy as np
 import pinocchio as pin
+from config import LEFT_HAND, RIGHT_HAND, LEFT_HOOK, RIGHT_HOOK, DT
+from tools import getcubeplacement
 
-from bezier import Bezier
-    
-# in my solution these gains were good enough for all joints but you might want to tune this.
-Kp = 20               # proportional gain (P of PD)
-Kv = 2 * np.sqrt(Kp)   # derivative gain (D of PD)
+# Constants:
+KpHP = 18000
+KiHP = 0.2 * KpHP
+KdHP = 2 * np.sqrt(KpHP)
+grasp_forceHP = 1800
 
-def controllaw(sim, robot, trajs, tcurrent, cube):
+
+# Constants:
+Kp = 25
+Ki = 0.01 * Kp 
+Kd = 2.5 * np.sqrt(Kp)
+grasp_force = 100*0
+INT_DECAY = 0.98
+PID_DECAY = 0.95
+
+def controllaw(sim, robot, trajs, tcurrent, tmax, cube, dt = DT):
+    global int_err 
     q, vq = sim.getpybulletstate()
+
+    pin.updateFramePlacements(robot.model, robot.data)
+    pin.framesForwardKinematics(robot.model, robot.data, q)
+
     
-    t_idx = (int) (tcurrent / dt)
-
-    q_desired = trajs[t_idx][0][:]
-    v_desired = trajs[t_idx][1][:]      # your array second dimension is actually velocity
-    # You need a_desired too - compute numerical derivative:
-    v_next = trajs[t_idx+1][1][:]
-    a_desired = (v_next - v_desired) / dt
+    # Get desired q, v, a
+    q_desired = trajs(tcurrent)
+    v_desired = trajs.derivative(1)(tcurrent)
+    a_desired = trajs.derivative(2)(tcurrent)
     
-    # PD correction (optional but recommended)
-    pos_err = q_desired - q
-    vel_err = v_desired - vq
+    # Calulcate PID error
+    q_err = q_desired - q
+    v_err = v_desired - vq
+    
+    # Decay of integral error
+    int_err *= INT_DECAY
+    int_err += q_err * dt 
 
-    a = a_desired + Kp*pos_err + Kv*vel_err
+    # Reduce gains when lowering the cube
+    t_lowering = tmax * 0.9
 
+    
+    if tcurrent > t_lowering:
+        Kp_phase = Kp * PID_DECAY
+        Ki_phase = Ki * PID_DECAY
+        Kd_phase = Kd * PID_DECAY
+        force_phase = grasp_force
+    else:
+        Kp_phase = Kp 
+        Ki_phase = Ki 
+        Kd_phase = Kd 
+        force_phase = grasp_force 
+
+    # a = a_desired + (Kp_phase * q_err) + (Kd_phase*v_err) + (Ki_phase * int_err)
+
+    # tau_g = pin.computeGeneralizedGravity(robotsim.model, robotsim.data, q)
+
+    # # Calculate basic Tau
+    # tau = pin.rnea(robot.model, robot.data, q, vq, a)#, [robomass]) #, [f_ext]) <---- Find adequate exterior forces e.g. mass of the robot
+    # tau_m = robot.data.tau.copy()
+
+    # tau_model = pin.rnea(robot.model, robot.data, q, vq, a_desired)
+
+    # PID correction torque
+    # tau_control = (Kp_phase * q_err) + (Kd_phase * v_err) + (Ki_phase * int_err)
+
+    # tau = tau_model + tau_control
+
+    tau = pin.rnea(robot.model, robot.data, q, np.zeros_like(q), np.zeros_like(q))
+
+    # Add "grasp force" so the cube does not get dropped
+    # For left hand
+    idx_L = robot.model.getFrameId(LEFT_HAND)
+    oML = robot.data.oMf[idx_L]
+    oMcubeL = getcubeplacement(cube, LEFT_HOOK)
+    left_err = oMcubeL.translation - oML.translation
+    # trans_err = oMcubeL.translation - oML.translation
+    # rot_err = 0.5 * pin.log3(oMcubeL.rotation @ oML.rotation.T)
+    # wrenchL = pin.Motion(np.hstack((trans_err * force_phase, rot_err)) )
+    wrenchL = pin.Motion(left_err * force_phase, np.zeros(3))
+    J_L = pin.computeFrameJacobian(robot.model, robot.data, q, idx_L, pin.LOCAL_WORLD_ALIGNED)
+
+    # For right hand
+    idx_R = robot.model.getFrameId(RIGHT_HAND)
+    oMR = robot.data.oMf[idx_R]
+    oMcubeR = getcubeplacement(cube, RIGHT_HOOK)
+    right_err = oMcubeR.translation - oMR.translation
+    # trans_err = oMcubeR.translation - oMR.translation
+    # rot_err = 0.5 * pin.log3(oMcubeR.rotation @ oMR.rotation.T)
+    # wrenchR = pin.Motion(trans_err * force_phase, rot_err)
+    wrenchR = pin.Motion(right_err * force_phase, np.zeros(3))
+    J_R = pin.computeFrameJacobian(robot.model, robot.data, q, idx_R, pin.LOCAL_WORLD_ALIGNED)
+
+    Rot_err = 0.5 * pin.log3(oMcubeL.rotation @ oML.rotation.T)
+    R_err = 0.5 * pin.log3(oMcubeR.rotation @ oMR.rotation.T)
+
+
+    tau_grasp = J_R.T @ wrenchR.vector - J_L.T @ wrenchL.vector
+
+    # Add grasp force to overall tau and step
+    print("q_err_norm:", np.linalg.norm(q_err), "v_err_norm:", np.linalg.norm(v_err))
+    print("int_err (max/min):", np.max(int_err), np.min(int_err))
+    print("tau_grasp (first 6):", tau_grasp[:6])
+    print("tau_total (first 6):", (tau + tau_grasp)[:6])
+
+    # tau += tau_grasp 
+    sim.step(tau)
+
+
+
+def controllawHighPID(sim, robot, trajs, tcurrent, tmax, cube, dt = DT):
+    global int_err 
+    q, vq = sim.getpybulletstate()
+
+    pin.updateFramePlacements(robot.model, robot.data)
+    pin.framesForwardKinematics(robot.model, robot.data, q)
+
+    
+    # Get desired q, v, a
+    q_desired = trajs(tcurrent)
+    v_desired = trajs.derivative(1)(tcurrent)
+    a_desired = trajs.derivative(2)(tcurrent)
+    
+    # Calulcate PID error
+    q_err = q_desired - q
+    v_err = v_desired - vq
+    
+    # Decay of integral error
+    int_err *= INT_DECAY
+    int_err += q_err * dt 
+
+    # Reduce gains when lowering the cube
+    t_lowering = tmax * 0.9
+
+    
+    if tcurrent > t_lowering:
+        Kp_phase = KpHP * PID_DECAY
+        Ki_phase = KiHP * PID_DECAY
+        Kd_phase = KdHP * PID_DECAY
+        force_phase = grasp_forceHP
+    else:
+        Kp_phase = KpHP
+        Ki_phase = KiHP 
+        Kd_phase = KdHP
+        force_phase = grasp_forceHP
+
+    a = a_desired + (Kp_phase * q_err) + (Kd_phase*v_err) + (Ki_phase * int_err)
+
+    # Calculate basic tau
+    # tau = pin.rnea(robot.model, robot.data, q, vq, a)#, [robomass]) #, [f_ext]) <---- Find adequate exterior forces e.g. mass of the robot
     tau = pin.rnea(robot.model, robot.data, q, vq, a)
 
-    print("tau:", tau)
+    # PID correction torque
+    # tau_control = (Kp_phase * q_err) + (Kd_phase * v_err) + (Ki_phase * int_err)
 
+
+    # Add "grasp force" so the cube does not get dropped
+    # For left hand
+    idx_L = robot.model.getFrameId(LEFT_HAND)
+    oML = robot.data.oMf[idx_L]
+    oMcubeL = getcubeplacement(cube, LEFT_HOOK)
+    trans_err = oMcubeL.translation - oML.translation
+    rot_err = 0.5 * pin.log3(oMcubeL.rotation @ oML.rotation.T)
+    wrenchL = pin.Motion(np.hstack((trans_err * force_phase, rot_err)) )
+    J_L = pin.computeFrameJacobian(robot.model, robot.data, q, idx_L, pin.LOCAL_WORLD_ALIGNED)
+
+    # For right hand
+    idx_R = robot.model.getFrameId(RIGHT_HAND)
+    oMR = robot.data.oMf[idx_R]
+    oMcubeR = getcubeplacement(cube, RIGHT_HOOK)
+    trans_err = oMcubeR.translation - oMR.translation
+    rot_err = 0.5 * pin.log3(oMcubeR.rotation @ oMR.rotation.T)
+    wrenchR = pin.Motion(trans_err * force_phase, rot_err)
+    J_R = pin.computeFrameJacobian(robot.model, robot.data, q, idx_R, pin.LOCAL_WORLD_ALIGNED)
+
+    tau_grasp = J_R.T @ wrenchR.vector - J_L.T @ wrenchL.vector
+
+    # Add grasp force to overall tau and step
+    tau += tau_grasp 
     sim.step(tau)
     
-    
-
-def controllawBezier(sim, robot, trajs, tcurrent, cube):
-    q, v = sim.getpybulletstate()
-    
-    t_idx = (int) (tcurrent)
-    print("t_idx", t_idx)
-
-    b = trajs
-
-    q_desired = b(tcurrent)
-    v_desired = b.derivative(1)(tcurrent)
-    a_desired = b.derivative(2)(tcurrent)
-    
-
-    # PD correction (optional but recommended)
-    a_cmd = a_desired #+ Kp*(q_desired - q) + Kv*(v_desired - v)
-    tau_des = pin.rnea(robot.model, robot.data, q_desired, v_desired, a_desired)
-    tau_err = Kp * (q_desired - q) + Kv * (v_desired - v) 
-    tau = tau_des + tau_err
-
-    print("tau:\n", tau)
-
-    sim.step(tau)
-
-    return tau
 
     
 
 if __name__ == "__main__":
         
     from tools import setupwithpybullet, setupwithpybulletandmeshcat, rununtil, setupwithmeshcat
-    from config import DT
     import time
+    from setup_meshcat import updatevisuals
     
     robotsim, sim, cubesim = setupwithpybullet()
     robot, cube, viz = setupwithmeshcat(url="tcp://127.0.0.1:6003")
     
     
-    from config import CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET    
+    from config import CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET
     from inverse_geometry import computeqgrasppose
     from path import computepath, displaypath
-    from trajectory import getTrajectory1, getTrajBezier
+    from trajectory import getTrajBezier
     
     q0,successinit = computeqgrasppose(robotsim, robotsim.q0, cubesim, CUBE_PLACEMENT, None)
     qe,successend = computeqgrasppose(robotsim, robotsim.q0, cubesim, CUBE_PLACEMENT_TARGET,  None)
-    path = computepath(robotsim, cubesim, q0, qe, CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET)
+    # path = computepath(robotsim, cubesim, q0, qe, CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET)
+    path = np.loadtxt('path.txt')
+
+    # path = computepath(robot, cube, q0, qrightup, CUBE_PLACEMENT, CUBE_PLACEMENT_RIGHT_UP)
 
     #setting initial configuration
     sim.setqsim(q0)
+    sim.setTorqueControlMode()
     
+
+    max_time = 1
+    num_of_steps = 1000
     
-    P, I, D = Kp, 1, Kv    
-    max_time = 10
-    num_of_steps = 100
-    
-    # trajs, time_steps = getTrajectory1(robot, cube, path, max_time, P, I, D)
     trajs, time_steps = getTrajBezier(robot, cube, path, max_time, num_of_steps)
-    
-    total_time = (int) (time_steps[-1])
 
     tcur = 0.
+    dt = max_time/(len(time_steps))
 
-    dt = max_time/(len(path)-2)
-
-    dt = max_time / num_of_steps
-
-    # print("traj",len(trajs))
-
-    displaypath(robot, cube, path, 1/500, viz)
+    # displaypath(robot, cube, path, 1/500, viz)
     
-    
-    while tcur < total_time:
-    #     # print("tcur:", tcur, "\tt_total:", total_time)
-    #     # print("control loop iteration:", (int) (tcur/dt), "of", (int) (total_time/dt))
+    int_err = np.zeros_like(trajs(0))
 
-    #     # rununtil(controllaw, dt, sim, robot, trajs, tcur, cube)
-    #     # tcur += dt
-        
-        controllawBezier(sim, robotsim, trajs, tcur, cubesim)
 
-    #     # rununtil(controllawBezier, DT*1000, sim, robot, trajs, tcur, cube)
+    while tcur < max_time:
+        controllawHighPID(sim, robotsim,trajs, tcur, max_time, cubesim, dt)
+        # rununtil(controllaw, sim, robotsim, trajs, tcur, cubesim, dt)
         tcur += dt
         time.sleep(0.01)
-    
-    
+    time.sleep(3)
     
--- trajectory.py
+++ trajectory.py
@@ -9,79 +9,6 @@ import time
 from setup_meshcat import updatevisuals
 
 
-
-def getTrajectory(robot, cube, path, max_time, P, I, D):
-    """
-    Get the trajectory given the path.
-    """
-    print("Getting Trajectory")
-    t_start = 0
-    t_end = max_time
-
-    # len(path) - 2 to make len(path) 1 longer than len(time_steps) 
-    dt = t_end/(len(path)-2)
-    time_steps = np.arange(t_start, t_end + dt, dt)
-
-    trajectories = np.empty([len(time_steps), 2, len(path[0])])
-
-    print(trajectories.shape)
-
-
-    for i in range(len(time_steps)-1):
-        # print("At time step" , i, "of", len(time_steps))
-        q_current = path[i]
-        q_current_goal = path[i+1]
-        # Configuration.
-        trajectories[i][0] = q_current
-        # Velocity/change.
-        trajectories[i][1] = q_current_goal - q_current
-
-        # trajectories += [stepThroughPath(robot, cube, q_current, q_current_goal, P, I, D)]
-
-    trajectories[-1][0] = path[-1]
-    trajectories[-1][0] = np.zeros(len(path[-1]))
-    return trajectories, time_steps
-
-
-def stepThroughPath(robot, cube, q_current, q_current_goal, k_p, k_i, k_d):
-
-    error = q_current_goal - q_current
-    q_prev = q_current
-    error_i = np.zeros(error.shape)
-    trajectories = []
-
-    while np.sum(np.abs(error))/2 > EPSILON:
-
-        error = q_current_goal - q_current
-        error_d = (error - (q_current_goal - q_prev)) / DT       
-        error_i += error * DT
-                
-        u = (k_p * error) + (k_d * error_d) + (k_i * error_i)
-
-        # Do I need to save q_current like this? I should probably sense q somehow
-        q_current = pin.integrate(robot.model, q_current, u*DT*5)
-        trajectories += [u]
-
-        data = robot.data.copy()  # Fresh data instance
-        pin.forwardKinematics(robot.model, data, q_current)
-        pin.updateFramePlacements(robot.model, data)  # Important!
-
-        # # For visuialiser
-        # effL = data.oMf[robot.model.getFrameId(LEFT_HAND)] 
-        # cube_pos = getcubeplacement(cube)
-        # eMc = effL.inverse() * cube_pos
-        
-        # cube_pos =  effL * eMc
-        # setcubeplacement(robot, cube, cube_pos)
-        # viz.display(q_current)
-
-        q_prev = q_current
-
-    return trajectories
-
-
-
-
 def getTrajBezier(robot, cube, path, max_time, num_of_steps):
     print("Getting Bezier Trajectory")
     t_start = 0
@@ -125,25 +52,6 @@ def getShortenedPath(path, num_of_steps):
     return shortened_path
 
 
-def makeBezTraj(q0,q1,t0, t1, mt):
-        b = Bezier([q0,q0,q1,q1], t0, t1, mt)
-        return b
-
-    
-
-
-# def displaypath(robot,cube,path,dt, viz):
-#     q_new = path[-1].copy()
-#     data = robot.data.copy()  # Fresh data instance
-#     pin.forwardKinematics(robot.model, data, q_new)
-#     pin.updateFramePlacements(robot.model, data)  # Important!
-#     frameL = robot.model.getFrameId(LEFT_HAND)
-#     effL = data.oMf[frameL] 
-#     cube_pos = getcubeplacement(cube)
-#     eMc = effL.inverse() * cube_pos
-
-
-
 if __name__ == "__main__":
     from tools import setupwithmeshcat
     from config import CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET
@@ -151,7 +59,7 @@ if __name__ == "__main__":
     from path import computepath, displaypath
     from bezier import Bezier
     
-    robot, cube, viz = setupwithmeshcat(url="tcp://127.0.0.1:6000")
+    robot, cube, viz = setupwithmeshcat(url="tcp://127.0.0.1:6003")
     
     q = robot.q0.copy()
     pin.framesForwardKinematics(robot.model,robot.data,q)
@@ -162,15 +70,23 @@ if __name__ == "__main__":
     if not(successinit and successend):
         print ("error: invalid initial or end configuration")
     
-    path = computepath(robot, cube, q0, qe, CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET)
+    # path = computepath(robot, cube, q0, qe, CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET)
+    path = np.loadtxt('path.txt')
 
 
     P, I, D = 10, 1, 1
-    max_time = 10
-    num_of_steps = 3
+    max_time = 30
+    num_of_steps = 30
     step_size = max_time / num_of_steps
 
     # trajs, time_steps = getTrajectory1(robot, cube, path, max_time, P, I, D)
 
-    trajs, time_steps = getTrajBezier(robot, cube, path, 10, num_of_steps)
+    trajs, time_steps = getTrajBezier(robot, cube, path, max_time, num_of_steps)
 
+    # Testing that the Belzier curve gives a path 
+    trajpath = []
+    traj_time_steps = np.arange(0, max_time, 0.1)
+    for t in traj_time_steps:
+        trajpath += [trajs(t)]
+
+    displaypath(robot, cube, trajpath, 1/10, viz)
